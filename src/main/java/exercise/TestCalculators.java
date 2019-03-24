bbcd06c2f10144ea5b9ad862e8c8daae90cf0f40
esahaghian <eduard.sahaghian@gmail.com>
2019-03-24 15:11:38 +0200

    Added more test cases.

--- src/main/java/exercise/TestCalculators.java
+++ src/main/java/exercise/TestCalculators.java
@@ -1,5 +1,8 @@
 package main.java.exercise;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static main.java.exercise.calculators.bmi.BodyMassIndexCalculator.bmiCalculator;
 import static main.java.exercise.calculators.bmr.BasalMetabolicRateCalculator.getBmr;
 import static main.java.exercise.calculators.bmr.BasalMetabolicRateCalculator.getCalories;
@@ -7,24 +10,37 @@ import static main.java.exercise.calculators.maxheartrate.MaxHeartRateCalculator
 
 public class TestCalculators {
     public static void main(String[] args) {
-        Person edi = new Person("Edi",90, 178, 40, Person.Gender.MALE, ActivityLevel.SEDENTARY);
+        Person edi = new Person("Edi", 90, 178, 40, Person.Gender.MALE, ActivityLevel.SEDENTARY);
+        Person alpar = new Person("Alpar", 74, 170, 31, Person.Gender.MALE, ActivityLevel.SEDENTARY);
+        Person raluca = new Person("Raluca", 62, 163, 34, Person.Gender.FEMALE, ActivityLevel.LIGHTLY_ACTIVE);
+        Person anca = new Person("Anca", 61, 165, 40, Person.Gender.FEMALE, ActivityLevel.MODERATELY_ACTIVE);
+        Person eric = new Person("Eric", 18, 110, 4, Person.Gender.MALE, ActivityLevel.SUPER_ACTIVE);
 
-        double bmrResult = getBmr(edi);
-        double caloriesResult = getCalories(edi);
+        List<Person> personList = new ArrayList<>();
+        personList.add(edi);
+        personList.add(alpar);
+        personList.add(raluca);
+        personList.add(anca);
+        personList.add(eric);
 
-        StringBuilder stringBuilder = new StringBuilder("Person's name: " + edi.getName());
-        stringBuilder.append("\n");
-        stringBuilder.append("BMI Classification: " + bmiCalculator(edi));
-        stringBuilder.append("\n");
-        stringBuilder.append("Max Heart Rate based on 3 formulas:");
-        stringBuilder.append("\n\t Formula 1 = " + getHrMaxOne(edi));
-        stringBuilder.append("\n\t Formula 2 = " + getHrMaxTwo(edi));
-        stringBuilder.append("\n\t Formula 3 = " + getHrMaxThree(edi));
-        stringBuilder.append("\n");
-        stringBuilder.append("BMR = " + bmrResult);
-        stringBuilder.append("\n");
-        stringBuilder.append("Necesar calorii zilnice = " + caloriesResult);
+        for (int i = 0; i < personList.size(); i++) {
+            double bmrResult = getBmr(personList.get(i));
+            double caloriesResult = getCalories(personList.get(i));
 
-        System.out.println(stringBuilder.toString());
+            StringBuilder stringBuilder = new StringBuilder("Person's name: " + personList.get(i).getName());
+            stringBuilder.append("\n");
+            stringBuilder.append("BMI Classification: " + bmiCalculator(personList.get(i)));
+            stringBuilder.append("\n");
+            stringBuilder.append("Max Heart Rate based on 3 formulas:");
+            stringBuilder.append("\n\t Formula 1 = " + getHrMaxOne(personList.get(i)));
+            stringBuilder.append("\n\t Formula 2 = " + getHrMaxTwo(personList.get(i)));
+            stringBuilder.append("\n\t Formula 3 = " + getHrMaxThree(personList.get(i)));
+            stringBuilder.append("\n");
+            stringBuilder.append("BMR = " + bmrResult);
+            stringBuilder.append("\n");
+            stringBuilder.append("Necesar calorii zilnice = " + caloriesResult);
+
+            System.out.println(stringBuilder.toString());
+        }
     }
 }
